246ba1f0f58cb9cb5f43501f74284427f39d7fd8
jacktheminecraftmodder <42350280+jacktheminecraftmodder@users.noreply.github.com>
2022-05-11 12:56:26 +0530

    update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import py
 import pygame
-
-# Event loop: 
+from sys import exit
 
 pygame.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('RPG Survival | Dungeon Run') # Gives a title to the window
 
-window = pygame.display.set_mode((800, 600)) # The tuple inside is (Width, Height)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # Draw all our elements and update everything
+    pygame.display.update() # This updates the display created above
